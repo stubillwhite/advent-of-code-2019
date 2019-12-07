627047d3c18bb094ccd605bd5f8dc1c01f5bad12
Stu W <stubillwhite@gmail.com>
2019-12-07 20:25:59 +0000

    Refactor instruction set

--- src/advent_of_code_2019/computer.clj
+++ src/advent_of_code_2019/computer.clj
@@ -32,72 +32,60 @@
 ;; Instruction set
 
 (ns-unmap *ns* 'execute-instruction)
-(defmulti execute-instruction (fn [x] (:opcode (current-instruction x))))
+(defmulti execute-instruction (fn [computer args mode] (:opcode (current-instruction computer))))
 
-;; TODO: Refactor all this
+(defmethod execute-instruction 1 [{:keys [ip prg] :as computer} [a b c] [a-mode b-mode c-mode]]
+  (assoc computer
+         :ip  (+ ip 4)
+         :prg (write-value 0 prg c (+ (read-value a-mode prg a)
+                                      (read-value b-mode prg b)))))
 
-(defmethod execute-instruction 1 [{:keys [ip prg] :as computer}]
-  (let [{:keys [a-mode b-mode]} (current-instruction computer)
-        [_ a b addr]            (drop ip prg)]
-    (assoc computer
-           :ip  (+ ip 4)
-           :prg (write-value 0 prg addr (+ (read-value a-mode prg a)
-                                           (read-value b-mode prg b))))))
+(defmethod execute-instruction 2 [{:keys [ip prg] :as computer} [a b c] [a-mode b-mode c-mode]]
+  (assoc computer
+         :ip  (+ ip 4)
+         :prg (write-value 0 prg c (* (read-value a-mode prg a)
+                                      (read-value b-mode prg b)))))
 
-(defmethod execute-instruction 2 [{:keys [ip prg] :as computer}]
-  (let [{:keys [a-mode b-mode]} (current-instruction computer)
-        [_ a b addr]            (drop ip prg)]
-    (assoc computer
-           :ip  (+ ip 4)
-           :prg (write-value 0 prg addr (* (read-value a-mode prg a)
-                                           (read-value b-mode prg b))))))
+(defmethod execute-instruction 3 [{:keys [ip prg stdin] :as computer} [a] [a-mode]]
+  (assoc computer
+         :ip    (+ ip 2)
+         :prg   (write-value 0 prg a (first stdin))
+         :stdin (rest stdin)))
 
-(defmethod execute-instruction 3 [{:keys [ip prg stdin] :as computer}]
-  (let [[_ addr] (drop ip prg)]
-    (assoc computer
-           :ip    (+ ip 2)
-           :prg   (write-value 0 prg addr (first stdin))
-           :stdin (rest stdin))))
+(defmethod execute-instruction 4 [{:keys [ip prg stdout] :as computer} [a] [a-mode]]
+  (assoc computer
+         :ip     (+ ip 2)
+         :stdout (conj stdout (read-value a-mode prg a))))
 
-(defmethod execute-instruction 4 [{:keys [ip prg stdout] :as computer}]
-  (let [{:keys [a-mode]} (current-instruction computer)
-        [_ a]            (drop ip prg)]
-    (assoc computer
-           :ip     (+ ip 2)
-           :stdout (conj stdout (read-value a-mode prg a)))))
+(defmethod execute-instruction 5 [{:keys [ip prg] :as computer} [a b] [a-mode b-mode]]
+  (assoc computer
+         :ip (if (not (zero? (read-value a-mode prg a)))
+               (read-value b-mode prg b)
+               (+ ip 3))))
 
-(defmethod execute-instruction 5 [{:keys [ip prg] :as computer}]
-  (let [{:keys [a-mode b-mode]} (current-instruction computer)
-        [_ a b]                 (drop ip prg)]
-    (assoc computer
-           :ip (if (not (zero? (read-value a-mode prg a)))
-                 (read-value b-mode prg b)
-                 (+ ip 3)))))
+(defmethod execute-instruction 6 [{:keys [ip prg] :as computer} [a b] [a-mode b-mode]]
+  (assoc computer
+         :ip (if (zero? (read-value a-mode prg a))
+               (read-value b-mode prg b)
+               (+ ip 3))))
 
-(defmethod execute-instruction 6 [{:keys [ip prg] :as computer}]
-  (let [{:keys [a-mode b-mode]} (current-instruction computer)
-        [_ a b]                 (drop ip prg)]
-    (assoc computer
-           :ip (if (zero? (read-value a-mode prg a))
-                 (read-value b-mode prg b)
-                 (+ ip 3)))))
+(defmethod execute-instruction 7 [{:keys [ip prg] :as computer} [a b c] [a-mode b-mode c-mode]]
+  (assoc computer
+         :prg (write-value 0 prg c (if (< (read-value a-mode prg a) (read-value b-mode prg b)) 1 0))
+         :ip  (+ ip 4)))
 
-(defmethod execute-instruction 7 [{:keys [ip prg] :as computer}]
-  (let [{:keys [a-mode b-mode]} (current-instruction computer)
-        [_ a b c]                (drop ip prg)]
-    (assoc computer
-           :prg (write-value 0 prg c (if (< (read-value a-mode prg a) (read-value b-mode prg b)) 1 0))
-           :ip  (+ ip 4))))
+(defmethod execute-instruction 8 [{:keys [ip prg] :as computer} [a b c] [a-mode b-mode c-mode]]
+  (assoc computer
+         :prg (write-value 0 prg c (if (= (read-value a-mode prg a) (read-value b-mode prg b)) 1 0))
+         :ip  (+ ip 4)))
 
-(defmethod execute-instruction 8 [{:keys [ip prg] :as computer}]
-  (let [{:keys [a-mode b-mode]} (current-instruction computer)
-        [_ a b c]                (drop ip prg)]
-    (assoc computer
-           :prg (write-value 0 prg c (if (= (read-value a-mode prg a) (read-value b-mode prg b)) 1 0))
-           :ip  (+ ip 4))))
-
-(defmethod execute-instruction 99 [{:keys [ip] :as computer}]
+(defmethod execute-instruction 99 [{:keys [ip] :as computer} [_] [_]]
   (assoc computer :halted? true))
+
+(defn- step [{:keys [ip prg] :as computer}]
+  (let [{:keys [a-mode b-mode c-mode] :as instr} (current-instruction computer)
+        [_ a b c] (drop ip prg)]
+    (execute-instruction computer [a b c] [a-mode b-mode c-mode])))
 
 ;; Public
 
@@ -114,7 +102,7 @@
   "Returns a lazy seq of all states of the computer until it halts."
   [computer]
   (take-while (fn [{:keys [ip halted?]}] (not halted?))
-              (iterate execute-instruction computer)))
+              (iterate step computer)))
 
 (defn execute-program
   "Returns the program in the final executed state."
