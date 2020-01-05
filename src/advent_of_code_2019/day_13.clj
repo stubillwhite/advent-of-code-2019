c81269ee9a29720e984a776811749514e473fc0a
Stu W <stubillwhite@gmail.com>
2020-01-05 18:07:37 +0000

    Add solution to day 13 part 2

--- src/advent_of_code_2019/day_13.clj
+++ src/advent_of_code_2019/day_13.clj
@@ -1,14 +1,18 @@
 (ns advent-of-code-2019.day-13
   (:require [advent-of-code-2019.computer :as cmp]
+            [advent-of-code-2019.computer-io :as cmp-io]
             [advent-of-code-2019.utils :refer [def- parse-long]]
             [clojure.java.io :as io]
             [clojure.string :as string]))
 
-(def- problem-input
+(def problem-input
   (string/trim (slurp (io/resource "day-13-input.txt"))))
 
-(defn create-game []
-  {:grid {}})
+(defn- create-game []
+  {:grid   {}
+   :paddle nil
+   :ball   nil
+   :score  0})
 
 (def- object-types
   {0 :empty
@@ -17,29 +21,55 @@
    3 :paddle
    4 :ball})
 
-(defn- object-of-type [id]
-  (object-types id))
+(defn- object-type [x y id]
+  (if (= [x y] [-1 0])
+    :score
+    (object-types id)))
 
-(defn- add-object [game x y id]
-  (assoc-in game [:grid [x y]] (object-of-type id)))
+(defn- update-attributes [game x y id]
+  (case (object-type x y id)
+    :score  (assoc game :score  id)
+    :paddle (assoc game :paddle [x y])
+    :ball   (assoc game :ball   [x y])
+    game))
+
+(defn- update-grid [game x y id]
+  (let [obj (object-type x y id)]
+    (case obj
+      :score game
+      (assoc-in game [:grid [x y]] obj))))
+
+(defn- update-state [game [x y id]]
+  (-> game
+      (update-attributes x y id)
+      (update-grid x y id)))
 
 (defn- create-game-state [cmds]
   (->> cmds
        (partition 3)
-       (reduce (partial apply add-object) (create-game))))
+       (reduce update-state (create-game))))
 
 (defn- count-blocks [{:keys [grid]}]
   (count (for [[k v] grid :when (= v :block)] k)))
 
+(defn solution-part-one [input]
+  (->> (cmp/initialise-computer (cmp/parse-program input))
+       (cmp/execute-program)
+       (cmp/flush-and-read-stdout)
+       (create-game-state)
+       (count-blocks)))
+
+;; Part two
+
 (defn- render-object [obj]
   (case obj
     :empty  " "
-    :wall   "#"
-    :block  "█"
-    :paddle "―"
-    :ball   "●"))
+    :wall   "|"
+    :block  "#"
+    :paddle "="
+    :ball   "o"))
 
-(defn render-game [{:keys [grid]}]
+(defn- render-grid [{:keys [grid]}]
   (let [width  (->> grid (keys) (map first) (apply max) (inc))
         height (->> grid (keys) (map last)  (apply max) (inc))]
     (->>
@@ -50,9 +80,41 @@
      (map (partial apply str))
      (string/join "\n"))))
 
-(defn solution-part-one [input]
-  (->> (cmp/initialise-computer (cmp/parse-program input))
-       (cmp/execute-program)
-       (cmp/flush-and-read-stdout)
-       (create-game-state)
-       (count-blocks)))
+(defn- render-game [{:keys [grid score] :as game}]
+  (let [height (->> grid (keys) (map last)  (apply max) (inc))]
+    (str (format "\nScore: %d\n" score)
+         (render-grid game)
+         (format "\033[%dA" (+ height 3)))))
+
+(ns-unmap *ns* 'GameIO)
+(defrecord GameIO [game cmd]
+  cmp-io/ComputerIO
+  (read-from-stdin [this]
+    (let [[bx _] (get-in this [:game :ball])
+          [px _] (get-in this [:game :paddle])
+          value  (cond
+                   (> bx px)  1
+                   (= bx px)  0
+                   (< bx px) -1)]
+      [value this]))
+  
+  (write-to-stdout [this v]
+    (let [cmd (conj (get-in this [:game :cmd]) v)]
+      (if (= 3 (count cmd))
+        (-> this
+            (update-in [:game] update-state cmd)
+            ;; ((fn [x] (println (render-game (:game x))) x))
+            (assoc-in  [:game :cmd] []))
+        (-> this
+            (update-in [:game :cmd] (fn [x] (conj x v))))))))
+
+(defn- game-io []
+  (->GameIO (create-game) []))
+
+(defn solution-part-two [input]
+  (let [prg (assoc (cmp/parse-program input) 0 2)]
+    (-> (cmp/initialise-computer prg :io (game-io))
+        (cmp/execute-program)
+        (get-in [:io :game :score]))))
+
+
--- test/advent_of_code_2019/day_13_test.clj
+++ test/advent_of_code_2019/day_13_test.clj
@@ -1,7 +1,7 @@
 (ns advent-of-code-2019.day-13-test
   (:require [advent-of-code-2019.day-13 :refer :all]
-            [clojure.test :refer :all]
-            [clojure.string :as string]))
+            [clojure.test :refer :all]))
 
-
+(deftest solution-part-one-given-problem-input-then-problem-result
+  (is (= 226 (solution-part-one problem-input))))
 
